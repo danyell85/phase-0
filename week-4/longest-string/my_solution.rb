d17f63675bbe6ce3234f99b2f5f2ce9978ee8b46
Danielle Cameron <danyell85@gmail.com>
2015-10-05 03:19:49 +0000

    Refactored Code

--- week-4/longest-string/my_solution.rb
+++ week-4/longest-string/my_solution.rb
@@ -1,3 +1,12 @@
+# def longest_string(list_of_words)
+# 	if list_of_words.length == 0
+# 		return nil
+# 	end
+# 	first_word = list_of_words[0]
+# 	list_of_words.each{|word| first_word = word if word.length > first_word.length}
+# 	first_word
+# end		
+
 def longest_string(list_of_words)
 	list_of_words.sort!{|x,y| y.length <=> x.length}
 	return list_of_words[0]
