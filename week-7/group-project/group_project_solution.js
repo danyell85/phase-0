cf41a06c94dac9a457ec67e5c27e956aeba8a076
Danielle Cameron <danyell85@gmail.com>
2015-10-23 17:13:51 +0000

    Correct Wording For User

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -2,8 +2,8 @@
 
 My role in the group is person 1: Test to User Stories
 
-As a user I want to be able to calculate the sum by going through an array and adding all the numbers in it as its own function.
+As a user I want to be able to calculate the sum by going through a list of numbers and adding all the numbers in it as its own function called sum.
 
-As a user I want to be able to calculate the mean by going through an array adding all the numbers and dividing by the length of the array as its function.
+As a user I want to be able to calculate the mean by going through a list of numbers, adding all the numbers and dividing by the total amount of numbers, as its function called mean.
 
-As a user I want to be able to calculate the median by going through an array and finding the middle number of the array as its own function.
+As a user I want to be able to calculate the median by going through a list of numbers and finding the middle number of that list, as its own function called median.
