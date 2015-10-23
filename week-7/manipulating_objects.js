4defda5c473d035f8d6466a30d0a9b8a43e53211
Danielle Cameron <danyell85@gmail.com>
2015-10-23 19:54:40 +0000

    Reflection and Tests

---
+++ week-7/manipulating_objects.js
@@ -0,0 +1,137 @@
+ // Manipulating JavaScript Objects
+
+// I worked on this challenge by myself for   hours.
+
+// There is a section below where you will write your code.
+
+// DO NOT ALTER THIS OBJECT BY ADDING ANYTHING WITHIN THE CURLY BRACES!
+var terah = {
+  name: "Terah",
+  age: 32,
+  height: 66,
+  weight: 130,
+  hairColor: "brown",
+  eyeColor: "brown"
+}
+// __________________________________________
+// Write your code below.
+var adam = {
+  name: "Adam",
+  spouse: terah,
+}
+terah.spouse = adam
+terah.weight = 125
+delete terah.eyeColor
+terah.children = {} // or new Object()
+terah.children.carson = { name: "Carson"}
+terah.children.carter = { name: "Carter"}
+terah.children.colton = { name: "Colton"}
+adam.children = terah.children
+// __________________________________________
+/* Reflection: Use the reflection guidelines
+What tests did you have trouble passing? What did you do to make it pass? Why did that work?
+I had trouble getting the children test to pass.  I wasnt adding the name property in the correct location. I thought I was adding a name property to childen and assigning it mulitple values like an array of the names.
+How difficult was it to add and delete properties outside of the object itself?
+The adding of properties was easy, it like how you create one inside an object but just assigning it a new value. I had to look up how to delete a property. 
+What did you learn about manipulating objects in this challenge?
+I learned you can create a new object by using empty curly braces instead of new Object(). I learned about nested objects. Like I statement above I thought it was all together instead of a new object with in the terah object.
+And if I am not explaining this correctly please correct me. 
+
+*/
+// __________________________________________
+// Driver Code:  Do not alter code below this line.
+function assert(test, message, test_number) {
+  if (!test) {
+    console.log(test_number + "false");
+    throw "ERROR: " + message;
+  }
+  console.log(test_number + "true");
+  return true;
+}
+
+assert(
+  (adam instanceof Object),
+  "The value of adam should be an Object.",
+  "1. "
+)
+
+assert(
+  (adam.name === "Adam"),
+  "The value of the adam name property should be 'Adam'.",
+  "2. "
+)
+
+assert(
+  terah.spouse === adam,
+  "terah should have a spouse property with the value of the object adam.",
+  "3. "
+)
+
+assert(
+  terah.weight === 125,
+  "The terah weight property should be 125.",
+  "4. "
+)
+
+assert(
+  terah.eyeColor === undefined || null,
+  "The terah eyeColor property should be deleted.",
+  "5. "
+)
+
+assert(
+  terah.spouse.spouse === terah,
+  "Terah's spouse's spouse property should refer back to the terah object.",
+  "6. "
+)
+
+assert(
+  (terah.children instanceof Object),
+  "The value of the terah children property should be defined as an Object.",
+  "7. "
+)
+
+assert(
+  (terah.children.carson instanceof Object),
+  "carson should be defined as an object and assigned as a child of Terah",
+  "8. "
+  )
+
+assert(
+  terah.children.carson.name === "Carson",
+  "Terah's children should include an object called carson which has a name property equal to 'Carson'.",
+  "9. "
+)
+
+assert(
+  (terah.children.carter instanceof Object),
+  "carter should be defined as an object and assigned as a child of Terah",
+  "10. "
+  )
+
+assert(
+  terah.children.carter.name === "Carter",
+  "Terah's children should include an object called carter which has a name property equal to 'Carter'.",
+  "11. "
+)
+
+assert(
+  (terah.children.colton instanceof Object),
+  "colton should be defined as an object and assigned as a child of Terah",
+  "12. "
+  )
+
+assert(
+  terah.children.colton.name === "Colton",
+  "Terah's children should include an object called colton which has a name property equal to 'Colton'.",
+  "13. "
+)
+
+assert(
+  adam.children === terah.children,
+  "The value of the adam children property should be equal to the value of the terah children property",
+  "14. "
+)
+
+console.log("\nHere is your final terah object:")
+console.log(terah)
