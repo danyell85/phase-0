776de4bebd2b5269e2e9918f33f820fe9fac535c
Danielle Cameron <danyell85@gmail.com>
2015-10-25 02:48:49 +0000

    Reflection

--- week-7/javascript.olympics.js
+++ week-7/javascript.olympics.js
@@ -20,20 +20,31 @@ var daniel = {
 var danielle ={
   name: "Danielle",
   height: "5.8",
-  sport: "running",
+  event: "running",
   quote: "Bye there"
 };
 
 // Bulk Up   x = "age"; person[x]
+// var array = [daniel, danielle];
+// var bulkUp = function(array){
+//   for(var x = 0; x <array.length; x++ ){
+//     var person = array[x];
+//     person.win = person.name + " won the " + person.sport + " event!";
+//   }
+// };
+// bulkUp(array);
+// console.log(danielle.win)
 var array = [daniel, danielle];
-var bulkUp = function(array){
+function bulkUp(array){
   for(var x = 0; x <array.length; x++ ){
     var person = array[x];
-    person.win = person.name + " won the " + person.sport + " event!";
+    person.win = person.name + " won the " + person.event + "!";
   }
 };
+
 bulkUp(array);
 console.log(danielle.win)
+console.log(danielle)
 // bulkUp(array)
 // console.log(daniel)
 // console.log(danielle)
@@ -42,18 +53,29 @@ console.log(danielle.win)
 
 
 // Jumble your words
-var jumble = function(string){
+// var jumble = function(string){
+//  return string.split("").reverse().join("");
+// };
+// console.log(jumble("Some string"));
+
+function jumble (string){
  return string.split("").reverse().join("");
 };
 console.log(jumble("Some string"));
 
 
 // 2,4,6,8
-var evens = function(array){
+// var evens = function(array){
+//  return array % 2 == 0;
+// };
+// var array = [1,2,3,4,5,7,8,9,10];
+// console.log(array.filter(evens));
+function evens(array){
  return array % 2 == 0;
 };
-array = [1,2,3,4,5,7,8,9,10];
+var array = [1,2,3,4,5,7,8,9,10];
 console.log(array.filter(evens));
+
 
 
 // "We built this city"
@@ -64,21 +86,17 @@ function Athlete(name, age , sport, quote){
   this.quote = quote;
 };
 
-
-
 var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
 var danielle = new Athlete(danielle.name,30, danielle.sport, danielle.quote);
 console.log(danielle)
 console.log(michaelPhelps.constructor === Athlete)
 console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
+/* Reflection
+What JavaScript knowledge did you solidify in this challenge?
+I wouldnt use the work solidify but it was good practice and showed where my weakness were.Assigning and calling properties on objects is much easier now. Still need practice fully grasping constructors.
+What are constructor functions?
+Constructor functions are objects that can be used in multiple instances to create new objects. or changed during its use 
+How are constructors different from Ruby classes (in your research)?
+From my poor understanding at this point, the difference is ruby objects inherite from a Class to create new ojects but javascript uses functions to create new objects. To inherit in javascript you need to use the prototype function.
 
-//Look into splat for args to not have to call each property.
-// function example(arg1, ...more_args) {
-//   console.log("Argument 1: ", arg1)
-//   console.log("Other arguments: ", more_args)
-// }
-
-// test_args = [1, 2, 3]
-
-// example(...test_args)
-// Reflection
+*/
